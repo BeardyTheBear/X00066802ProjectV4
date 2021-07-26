d9ee1bb04978b58b1d340fb98d73be072d6bb2e0
BeardyTheBear <55627116+BeardyTheBear@users.noreply.github.com>
2021-07-26 19:45:36 +0100

    Added login, some CSS work. Needs more work

--- FrontEnd/javascript/script.js
+++ FrontEnd/javascript/script.js
@@ -1,2 +1,10 @@
 const BASE_URL = `http://localhost:8080`;
 
+//var userLevel = "user";
+
+//if (userLevel = "user") {
+//    document.getElementById("fundsTable").style.visibility = "hidden";
+//}
+//else {
+//    document.getElementById("fundsTable").style.visibility = "true";
+//};
