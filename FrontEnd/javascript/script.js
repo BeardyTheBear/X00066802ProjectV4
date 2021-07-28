ffa95eb1b5540f86acb2c79ec7d2d75b69eca0e6
BeardyTheBear <55627116+BeardyTheBear@users.noreply.github.com>
2021-07-28 19:54:10 +0100

    more login edits, fixed css, test script

--- FrontEnd/javascript/script.js
+++ FrontEnd/javascript/script.js
@@ -1,10 +1,13 @@
-const BASE_URL = `http://localhost:8080`;
+//const BASE_URL = `http://localhost:8080`;
 
-//var userLevel = "user";
+var username_element = document.getElementById("username - field");
+var password_element = document.getElementById("password-field");
 
-//if (userLevel = "user") {
-//    document.getElementById("fundsTable").style.visibility = "hidden";
-//}
-//else {
-//    document.getElementById("fundsTable").style.visibility = "true";
-//};
+function formTest() {
+    if (username_element == password_element) {
+        console.log("Wow it is working")
+    }
+};
+
+document.getElementById("login-form-submit").addEventListener("click", formTest);
+
